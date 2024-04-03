8a51f1bcfcbb0a447af19f0a589dbb7a0d914452
Olga Barbato <olga.barbato@gmail.com>
2024-04-03 14:24:18 +0200

    feat:schemas configuration

---
+++ final-project/src/models/daily.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const dailySchema = new mongoose.Schema({
+  userId: { type: String, required: true },
+  title: { type: String, required: true },
+  complete: { type: Boolean, default: false },
+  lastCompleted: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Daily", dailySchema);
