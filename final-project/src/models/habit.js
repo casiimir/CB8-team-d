3181ad88c1821e2f59500b7b470c742a677e3538
Olga Barbato <olga.barbato@gmail.com>
2024-04-03 14:24:18 +0200

    feat:schemas configuration

---
+++ final-project/src/models/habit.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const habitSchema = new mongoose.Schema({
+  userId: { type: String, required: true },
+  title: { type: String, required: true },
+  streak: { type: Number, default: 0 },
+  lastCompleted: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Habit", habitSchema);
