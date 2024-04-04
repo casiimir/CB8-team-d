cb5a8419c2156529f7e1b4f5d718b98a1d5a366c
Olga Barbato <olga.barbato@gmail.com>
2024-04-04 15:20:32 +0200

    fine CRUD

--- src/pages/api/dailies/[id].js
+++ src/pages/api/dailies/[id].js
@@ -1,2 +1,57 @@
 import dbConnect from "@/utils/dbConnect";
 import Daily from "@/models/daily";
+
+export default async function handler(req, res) {
+  const { method, query } = req;
+
+  console.log(query.id);
+
+  await dbConnect();
+
+  switch (method) {
+    case "GET":
+      try {
+        const daily = await Daily.findById(query.id);
+
+        if (!daily) {
+          return res.status(400).json({ success: false });
+        }
+
+        res.status(200).json({ success: true, data: habit });
+      } catch (error) {
+        return res.status(400).json({ success: false });
+      }
+      break;
+
+    case "PUT":
+      try {
+        const { body } = req;
+        const daily = await Daily.findByIdAndUpdate(query.id, body);
+
+        if (!daily) {
+          return res.status(404).json({ success: false });
+        }
+
+        res.status(200).json({ success: true, data: daily });
+      } catch (error) {
+        return res.status(400).json({ success: false });
+      }
+      break;
+
+    case "DELETE":
+      try {
+        const deletedDaily = await Daily.deleteOne({ _id: query.id });
+
+        if (!deletedDaily) {
+          return res.status(404).json({ success: false });
+        }
+
+        res.status(204).json({ success: true, data: {} });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+    default:
+      res.setHeader("Allow", ["GET", "PUT", "DELETE"]);
+      res.status(405).end(`Metodo ${method} non accettato!`);
+  }
+}
