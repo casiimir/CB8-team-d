cb803f15e854c49f5aa22198836775ba4353e852
Elena Faraci <elena.faraci60@gmail.com>
2024-04-05 15:55:59 +0200

    navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,78 @@
+import styles from "./index.module.scss";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import {
+  FaTree,
+  FaCircleNotch,
+  FaCalendarCheck,
+  FaCheckCircle,
+} from "react-icons/fa";
+import { FaPlus } from "react-icons/fa6";
+
+const Navbar = () => {
+  const pathName = usePathname();
+
+  return (
+    <>
+      <nav className={styles.navbar}>
+        <ul className={styles.packIcons}>
+          <li>
+            <Link
+              href="/habits"
+              className={pathName === "/habits" ? styles.active : ""}
+            >
+              {" "}
+              {pathName === "/habits" ? (
+                <FaCircleNotch className={styles.active} />
+              ) : (
+                <FaCircleNotch className={styles.icon} />
+              )}
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/dailies"
+              className={pathName === "/dailies" ? styles.active : ""}
+            >
+              {" "}
+              {pathName === "/dailies" ? (
+                <FaCalendarCheck className={styles.active} />
+              ) : (
+                <FaCalendarCheck className={styles.icon} />
+              )}
+            </Link>
+          </li>
+        </ul>
+        <ul className={styles.packIcons}>
+          <li>
+            <Link
+              href="/todos"
+              className={pathName === "/todos" ? styles.active : ""}
+            >
+              {pathName === "/todos" ? (
+                <FaCheckCircle className={styles.active} />
+              ) : (
+                <FaCheckCircle className={styles.icon} />
+              )}
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/garden"
+              className={pathName === "/garden" ? styles.active : ""}
+            >
+              {pathName === "/garden" ? (
+                <FaTree className={styles.active} />
+              ) : (
+                <FaTree className={styles.icon} />
+              )}
+            </Link>
+          </li>
+        </ul>
+        <FaPlus className={styles.addBtn} />
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
