dc96f16594936c655423eccab956581051730544
Olga Barbato <olga.barbato@gmail.com>
2024-04-10 18:06:14 +0200

    feat/prova Json per lo shop degli alberi

--- src/components/gardenModal/GardenModal.jsx
+++ src/components/gardenModal/GardenModal.jsx
@@ -1,24 +1,38 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import styles from "./index.module.scss";
 import { IoWaterOutline, IoClose } from "react-icons/io5";
 import { FaCheck } from "react-icons/fa6";
 
-const plantsData = [
-  {
-    name: "Pianta 1",
-    imageUrl: "https://img.icons8.com/bubbles/50/potted-plant.png",
-  },
-  {
-    name: "Pianta 2",
-    imageUrl: "https://img.icons8.com/clouds/100/potted-plant.png",
-  },
-];
+// const plantsData = [
+//   {
+//     name: "Pianta 1",
+//     imageUrl: "https://img.icons8.com/bubbles/50/potted-plant.png",
+//   },
+//   {
+//     name: "Pianta 2",
+//     imageUrl: "https://img.icons8.com/clouds/100/potted-plant.png",
+//   },
+// ];
 
 const GardenModal = ({ onClose, onPlantSelect }) => {
+  const [trees, setTrees] = useState([]);
+
   const handlePlantSelect = (plantIconUrl) => {
     onPlantSelect(plantIconUrl);
     onClose();
   };
+
+  useEffect(() => {
+    fetch("trees.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setTrees(data);
+      })
+      .catch((error) =>
+        console.error("Errore nel caricamento del file JSON:", error)
+      );
+  }, []);
 
   return (
     <>
@@ -28,8 +42,8 @@ const GardenModal = ({ onClose, onPlantSelect }) => {
             <button className={styles.closeBtn} onClick={onClose}>
               <IoClose className={styles.closeIcon} />
             </button>
-            <h3 className={styles.modalTitle}>Aggiungi una nuova pianta</h3>
-            <div className={styles.shop}>
+            <h3 className={styles.modalTitle}>Aggiungi un nuovo albero</h3>
+            {/* <div className={styles.shop}>
               {plantsData.map((plant, index) => (
                 <div key={index} className={styles.plant}>
                   <p className={styles.name}>{plant.name}</p>
@@ -50,13 +64,37 @@ const GardenModal = ({ onClose, onPlantSelect }) => {
                         <IoWaterOutline />2
                       </p>
                     </div>
-                  </div>
-                  <button
+                  </div> */}
+            {/* <button
                     className={styles.addBtn}
                     onClick={() => handlePlantSelect(plant.imageUrl)}
-                  >
-                    <FaCheck className={styles.addIcon} />
+                  > */}
+            {/* <FaCheck className={styles.addIcon} />
                   </button>
+                </div>
+              ))}
+            </div> */}
+            <div className={styles.shop}>
+              {trees.map((tree, index) => (
+                <div key={index} className={styles.plant}>
+                  <p className={styles.name}>{tree.name}</p>
+                  <div className={styles.plantInfo}>
+                    <img
+                      width="50"
+                      height="50"
+                      src={tree.sprite}
+                      alt={tree.name}
+                    />
+                    <div>
+                      <p>{tree.cost.soil} soil</p>
+                      <p>{tree.cost.seeds} seeds</p>
+                      <p>{tree.cost.water} water</p>
+                    </div>
+                    <button
+                      className={styles.addBtn}
+                      onClick={() => handlePlantSelect(tree.sprite)}
+                    ></button>
+                  </div>
                 </div>
               ))}
             </div>
