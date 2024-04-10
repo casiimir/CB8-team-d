dddfacfafc864f372d71a4606b69ae48ec86ad34
perezandr <andrea.perez.pa@gmail.com>
2024-04-10 13:39:25 +0200

    aggiunto dominio imagini a nex.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["media.istockphoto.com"], //dominio delle immagini
+    domains: ["media.istockphoto.com", "static.vecteezy.com"], //dominio delle immagini
   },
 };
 
