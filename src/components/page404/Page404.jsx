8c7f60fafe10277f3ea53b0f31f1516846daf2f5
Euphyre <feufemia@hotmail.it>
2024-04-14 16:10:21 +0200

    cambiamenti 404, login e signup colori

--- src/components/page404/Page404.jsx
+++
@@ -1,9 +0,0 @@
-const Page404 = () => {
-  return (
-    <div>
-      <p> Sorry this page can not be found</p>
-    </div>
-  );
-};
-
-export default Page404;
---
+++ src/pages/404.jsx
@@ -0,0 +1,32 @@
+import styles from "@/styles/Custom404.module.scss";
+import Image from "next/image";
+import { useRouter } from "next/router";
+
+export default function Custom404() {
+  const router = useRouter();
+  return (
+    <div className={styles.notfound_wrapper}>
+      <div className={styles.items_wrapper}>
+        <Image
+          className={styles.image}
+          src="/bare tree.png"
+          alt=" bare tree"
+          width="200"
+          height="200"
+        />
+        <h1 className={styles.title}>
+          <span className={styles.highlight}> Oops!</span> It seems that nothing{" "}
+          grows here!
+        </h1>
+      </div>
+      <div className={styles.button_wrapper}>
+        <button
+          className={styles.button_login}
+          onClick={() => router.push("/")}
+        >
+          Home
+        </button>
+      </div>
+    </div>
+  );
+}
