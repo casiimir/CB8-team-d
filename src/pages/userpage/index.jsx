28f58d4815298846a6e8fb80f8e63476fa105051
Valentina Scalone <vallish1988@gmail.com>
2024-04-14 22:34:04 +0200

    fix userpage and css tasklist

--- src/pages/userpage/index.jsx
+++ src/pages/userpage/index.jsx
@@ -1,17 +1,25 @@
 import React, { useState } from "react";
 import Image from "next/image";
-import styles from "../../styles/Userpage.module.scss";
+import styles from "../../styles/userpage.module.scss";
+import { getSession, useSession } from "next-auth/react";
+import { useUserResources } from "@/contexts/userResourcesContext";
+import Navbar from "@/components/navbar";
+import Header from "@/components/header";
 
 export default function UserProfilePage() {
-  const [user, setUser] = useState({
-    name: "Pippo",
-    surname: "Pollo",
-    username: "Pippollo5798",
-    email: "pippolloe@pippo.com",
-    imageUrl:
-      "https://static.vecteezy.com/system/resources/previews/019/633/059/original/8-bit-pixel-human-portrait-cartoon-young-girl-for-game-assets-in-illustration-vector.jpg",
-  });
-
+  // const [user, setUser] = useState({
+  //   name: "Pippo",
+  //   surname: "Pollo",
+  //   username: "Pippollo5798",
+  //   email: "pippolloe@pippo.com",
+  //   imageUrl:
+  //     "https://static.vecteezy.com/system/resources/previews/019/633/059/original/8-bit-pixel-human-portrait-cartoon-young-girl-for-game-assets-in-illustration-vector.jpg",
+  // });
+  const { data: session, status: sessionStatus } = useSession();
+  const user = session?.user?.username;
+  const imageUrl =
+    "https://static.vecteezy.com/system/resources/previews/019/633/059/original/8-bit-pixel-human-portrait-cartoon-young-girl-for-game-assets-in-illustration-vector.jpg";
+  
   const [editing, setEditing] = useState(false);
 
   const onHandleChange = (e) => {
@@ -37,90 +45,73 @@ export default function UserProfilePage() {
   };
 
   return (
-    <div className={styles.userpage_wrapper}>
-      <div className={styles.image_wrapper}>
-        <Image
-          className={styles.image}
-          src={user.imageUrl}
-          priority={false}
-          alt="User Image"
-          width="200"
-          height="200"
-        />
-      </div>
-      <form onSubmit={onHandleSubmit} className={styles.submit_form}>
-        {/* <div>
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={user.name}
-            onChange={onHandleChange}
-            disabled={!editing}
+    <div>
+      <Header />
+      <div className={styles.userpage_wrapper}>
+        <div className={styles.image_wrapper}>
+          <Image
+            className={styles.image}
+            src={imageUrl}
+            priority={false}
+            alt="User Image"
+            width="150"
+            height="150"
           />
         </div>
-        <div>
-          <label htmlFor="surname">Surname:</label>
-          <input
-            type="text"
-            id="surname"
-            name="surname"
-            value={user.surname}
-            onChange={onHandleChange}
-            disabled={!editing}
-          />
-        </div> */}
-        <div>
-          <label htmlFor="username" className={styles.label}>
-            Username
-          </label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={user.username}
-            onChange={onHandleChange}
-            disabled={!editing}
-          />
-        </div>
-        <div>
-          <label htmlFor="email" className={styles.label}>
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={user.email}
-            onChange={onHandleChange}
-            disabled={!editing}
-          />
-        </div>
-        <div className={styles.btn_wrapper}>
-          {editing ? (
-            <button type="submit" className={styles.btn_sub}>
-              Save Account
-            </button>
-          ) : (
+        <form onSubmit={onHandleSubmit} className={styles.submit_form}>
+          <div>
+            <label htmlFor="username" className={styles.label}>
+              Username
+            </label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={session?.user?.username}
+              onChange={onHandleChange}
+              disabled={!editing}
+            />
+          </div>
+          <div>
+            <label htmlFor="email" className={styles.label}>
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={session?.user?.email}
+              onChange={onHandleChange}
+              disabled={!editing}
+            />
+          </div>
+          <div className={styles.btn_wrapper}>
+            {editing ? (
+              <button type="submit" className={styles.btn_sub}>
+                Save Account
+              </button>
+            ) : (
+              <button
+                type="button"
+                onClick={onHandleEdit}
+                className={styles.btn_edit}
+              >
+                Edit Account
+              </button>
+            )}
+            <br />
             <button
               type="button"
-              onClick={onHandleEdit}
-              className={styles.btn_edit}
+              onClick={onHandleDelete}
+              className={styles.btn_del}
             >
-              Edit Account
+              Delete Account
             </button>
-          )}
-          <br />
-          <button
-            type="button"
-            onClick={onHandleDelete}
-            className={styles.btn_del}
-          >
-            Delete Account
-          </button>
-        </div>
-      </form>
+          </div>
+        </form>
+      </div>
+      <Navbar />
     </div>
   );
 }
+ 
