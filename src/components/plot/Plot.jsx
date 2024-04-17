0912b8469bc1e49c501f9b6e2d387ccf98c9ede8
perezandr <andrea.perez.pa@gmail.com>
2024-04-17 19:22:47 +0200

    animazioni task e alberi

--- src/components/plot/Plot.jsx
+++ src/components/plot/Plot.jsx
@@ -2,6 +2,7 @@ import styles from "./index.module.scss";
 import { IoClose } from "react-icons/io5";
 import { LuAxe } from "react-icons/lu";
 import Image from "next/image";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Plot = ({
   x,
@@ -21,33 +22,42 @@ const Plot = ({
     plotToRemove && plotToRemove.x === x && plotToRemove.y === y;
 
   return (
-    <div
-      className={`${styles.plot} ${isEmpty ? styles.empty : styles.occupied}`}
-      onClick={handleClick}
-    >
-      {!isEmpty && (
-        <>
-          <Image
-            width="50"
-            height="50"
-            className={styles.plantIcon}
-            src={plantIcon}
-            alt="Plant"
-          />
-          {showRemoveButton && (
-            <div className={styles.buttonWrapper}>
-              <button onClick={() => onRemove()} className={styles.removeBtn}>
-                <LuAxe className={styles.removeIcon} />
-              </button>
-              <button onClick={() => onCancel()} className={styles.closeBtn}>
-                {" "}
-                <IoClose className={styles.closeIcon} />
-              </button>
-            </div>
-          )}
-        </>
-      )}
-    </div>
+    <AnimatePresence>
+      <div
+        className={`${styles.plot} ${isEmpty ? styles.empty : styles.occupied}`}
+        onClick={handleClick}
+      >
+        {!isEmpty && (
+          <>
+            <motion.div
+              className={styles.plantAnimation}
+              initial={{ y: 50, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              exit={{ y: 100, opacity: 0 }}
+            >
+              <Image
+                width="50"
+                height="50"
+                className={styles.plantIcon}
+                src={plantIcon}
+                alt="Plant"
+              />
+            </motion.div>
+            {showRemoveButton && (
+              <div className={styles.buttonWrapper}>
+                <button onClick={() => onRemove()} className={styles.removeBtn}>
+                  <LuAxe className={styles.removeIcon} />
+                </button>
+                <button onClick={() => onCancel()} className={styles.closeBtn}>
+                  {" "}
+                  <IoClose className={styles.closeIcon} />
+                </button>
+              </div>
+            )}
+          </>
+        )}
+      </div>
+    </AnimatePresence>
   );
 };
 
